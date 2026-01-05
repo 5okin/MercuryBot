3f6e1ff12c54ba9ac359d4f29b2cc77f3f007b0d
Nikos Koutsolelos <70406237+5okin@users.noreply.github.com>
2026-01-05 16:02:51 +0000

    refactor(messages): improve default message handling

--- clients/discord/messages.py
+++ clients/discord/messages.py
@@ -2,6 +2,7 @@ import discord
 
 
 footer = ""
+ZWSP = "\u200B" # Zero Width Space
 
 #MARK: epic
 def epic(store, mobile=False):
@@ -16,30 +17,30 @@ def epic(store, mobile=False):
 
         if deal['activeDeal']:
             now_end_date = store.get_date(deal, 'end', True)
-            all_freenow += "• " + f"[**{title}**]({link})\n‎"
+            all_freenow += "• " + f"[**{title}**]({link})\n{ZWSP}"
         else:
             start_date = store.get_date(deal, 'start')
             end_date = store.get_date(deal, 'end')
 
             game_details = f"Free: {start_date} - {end_date}"
-            all_upnext += "• " + f"[**{title}**]({link})‎\n"
+            all_upnext += "• " + f"[**{title}**]({link}){ZWSP}\n"
 
-    embed_var.add_field(name=f'\u200B\n**Free Now**', value=f"Until: {now_end_date}\n\n{all_freenow}", inline=True)
+    embed_var.add_field(name=f'{ZWSP}\n**Free Now**', value=f"Until: {now_end_date}\n\n{all_freenow}", inline=True)
     if (not mobile):
-        embed_var.add_field(name=f'\u200B\n**Up Next**', value=f"‎‎‎{game_details}\n\n{all_upnext}‎", inline=True)
+        embed_var.add_field(name=f'{ZWSP}\n**Up Next**', value=f"{ZWSP}{ZWSP}{ZWSP}{game_details}\n\n{all_upnext}{ZWSP}", inline=True)
     embed_var.set_footer(text=footer)
 
     return embed_var
 
 # MARK: gog
 def gog(store, mobile=False):
-    embed_var = discord.Embed(title="🕹️ GOG 🕹️", description=f'\u200B\n**Free Now**', color=0x00aff4)
+    embed_var = discord.Embed(title="🕹️ GOG 🕹️", description=f'{ZWSP}\n**Free Now**', color=0x00aff4)
 
     for deal in store.data:
         title = deal['title']
         link = deal['url']
         end_date = store.get_date(deal, 'end', True)
-        embed_var.add_field(name=f'\u200B\n', value=f"[**{title}**]({link})\nUntil: {end_date}", inline=False)
+        embed_var.add_field(name=f'{ZWSP}\n', value=f"[**{title}**]({link})\nUntil: {end_date}", inline=False)
 
     embed_var.set_image(url="attachment://img.gif")
     embed_var.set_footer(text=footer)
@@ -47,39 +48,43 @@ def gog(store, mobile=False):
 
 # MARK: default
 def default(store, mobile=False):
-    embed_var = discord.Embed(title=f"🕹️ {store.service_name} 🕹️", description="\u200B\n**Free now**", color=0x00aff4)
-    col1 = col2 = ""
+    DISCORD_MAX_FIELD_VALUE_CHARS = 1024
+    NUM_OF_DEALS_USE_SINGLE_COLUMN = 5
+    USE_SINGLE_COLUMN = len(store.data) <= NUM_OF_DEALS_USE_SINGLE_COLUMN
+    col1 = []
+    col2 = []
 
-    def add_row():
+    embed_var = discord.Embed(title=f"🕹️ {store.service_name} 🕹️", description=f"{ZWSP}\n**Free now**", color=0x00aff4)
+
+    def save_row():
+        nonlocal col1, col2
+
         if not col1 and not col2:
             return
 
-        embed_var.add_field(name="\u200B\n", value=col1 or "\u200B", inline=True)
-        embed_var.add_field(name="\u200B", value="\u200B", inline=True)
-        embed_var.add_field(name="\u200B", value=col2 or "\u200B", inline=True)
+        embed_var.add_field(name=f"{ZWSP}\n", value="".join(col1) or ZWSP, inline=True)
+        embed_var.add_field(name=ZWSP, value=f"{ZWSP}", inline=True)
+        embed_var.add_field(name=ZWSP, value="".join(col2) or ZWSP, inline=True)
+
+        col1.clear()
+        col2.clear()
 
     for i, deal in enumerate(store.data):
-        title = deal['title']
-        link = deal['url']
+        entry = f"• [**{deal['title']}**]({deal['url']})"
         end_date = store.get_date(deal, 'end', True)
-
-        entry = f"• [**{title}**]({link})"
         if end_date:
             entry += f"\nUntil: {end_date}"
         entry += "\n\n"
 
-        target = col1 if i % 2 == 0 else col2
+        target = col1 if USE_SINGLE_COLUMN or i % 2 == 0 else col2
 
-        if len(target) + len(entry) >= 1024:
-            add_row()
-            col1 = col2 = ""
+        # Check if adding this entry would exceed Discord's field character limit
+        if sum(len(e) for e in target) + len(entry) >= DISCORD_MAX_FIELD_VALUE_CHARS:
+            save_row()
 
-        if i % 2 == 0:
-            col1 += entry
-        else:
-            col2 += entry
+        target.append(entry)
 
-    add_row()
+    save_row()
     embed_var.set_image(url="attachment://img.gif")
     embed_var.set_footer(text=footer)
     return embed_var
